380099b22e237c5887934f20afb012dee0316b17
Olivier Charon <olivier.charon@outlook.fr>
2020-08-29 16:43:21 +0200

    store part

--- pages/posts/index.vue
+++ pages/posts/index.vue
@@ -10,33 +10,39 @@ export default {
   components: {
     PostList,
   },
-  asyncData(context) {
+  fetch(context) {
     return new Promise((resolve, reject) => {
-      //   resolve({
-      //     loadedPosts: [
-      //       {
-      //         id: '1',
-      //         title: 'First post',
-      //         previewText: 'This is our first post!',
-      //         thumbnail:
-      //           'https://tra.img.pmdstatic.net/fit/https.3A.2F.2Fi.2Einsider.2Ecom.2F5f2c4c6f988ee36b2d52d965/812x609/background-color/ffffff/quality/70/elon-musk-poem-tweets-gpt-3-openai-2020-8.jpg',
-      //       },
-      //       {
-      //         id: '2',
-      //         title: 'Second post',
-      //         previewText: 'This is our second post!',
-      //         thumbnail:
-      //           'https://tra.img.pmdstatic.net/fit/https.3A.2F.2Fi.2Einsider.2Ecom.2F5f2c4c6f988ee36b2d52d965/812x609/background-color/ffffff/quality/70/elon-musk-poem-tweets-gpt-3-openai-2020-8.jpg',
-      //       },
-      //     ],
-      reject(new Error())
+      resolve({
+        loadedPosts: [
+          {
+            id: '1',
+            title: 'First post',
+            previewText: 'This is our first post!',
+            thumbnail:
+              'https://tra.img.pmdstatic.net/fit/https.3A.2F.2Fi.2Einsider.2Ecom.2F5f2c4c6f988ee36b2d52d965/812x609/background-color/ffffff/quality/70/elon-musk-poem-tweets-gpt-3-openai-2020-8.jpg',
+          },
+          {
+            id: '2',
+            title: 'Second post',
+            previewText: 'This is our second post!',
+            thumbnail:
+              'https://tra.img.pmdstatic.net/fit/https.3A.2F.2Fi.2Einsider.2Ecom.2F5f2c4c6f988ee36b2d52d965/812x609/background-color/ffffff/quality/70/elon-musk-poem-tweets-gpt-3-openai-2020-8.jpg',
+          },
+        ],
+        // reject(new Error())
+      })
+        .then((data) => {
+          context.store.commit('setPosts', data.loadedPosts)
+        })
+        .catch((e) => {
+          context.error(e)
+        })
     })
-      .then((data) => {
-        return data
-      })
-      .catch((e) => {
-        context.error(e)
-      })
+  },
+  computed: {
+    loadedPosts() {
+      return this.$store.getters.loadedPosts
+    },
   },
 }
 </script>
